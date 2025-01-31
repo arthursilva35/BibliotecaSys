c89a6b3a5ebe45a360db424024f5c9c8bd09f7fe
arthursilva35 <arthurss802@gmail.com>
2025-01-30 21:33:25 -0300

    retirando implementacao dos metodos

--- IUsuario.py
+++ IUsuario.py
@@ -1,40 +1,32 @@
 from abc import ABC, abstractmethod
 
 class IUsuario(ABC):
-    def __init__(self, id, nome, regra_emprestimo):
-        self._id = id
-        self._nome = nome
-        self._esta_devendo = False
-        self._emprestimos = []
-        self._reservas = []
-        self._regra_emprestimo = regra_emprestimo
-
+    
+    @abstractmethod
     def get_id(self):
-        return self._id
+        pass
+        
     
+    @abstractmethod
     def get_nome(self):
-        return self._nome
+        pass
+      
     
+    @abstractmethod
     def get_esta_devendo(self):
-        return self._esta_devendo
+        pass
 
+
+    @abstractmethod
     def get_emprestimos(self):
-        return self._emprestimos
+        pass
+
     
+    @abstractmethod
     def get_reservas(self):
-        return self._reservas
+        pass
+
     
-    def set_id(self, id):
-        self._id = id
-
-    def set_nome(self, nome):
-        self._nome = nome
-
-    def set_esta_devendo(self):
-        pass
-
-    def solicitar_emprestimo(self):
-        pass
-
-    def solicitar_reserva(self):
+    @abstractmethod
+    def mudar_situacao_devedor(self):
         pass
