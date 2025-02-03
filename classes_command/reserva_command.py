efedc8bcff8934d9a5f585e45f28f1fa49a72af4
arthursilva35 <arthurss802@gmail.com>
2025-02-03 13:41:10 -0300

    implementando comando pra reserva

--- classes_command/reserva_command.py
+++ classes_command/reserva_command.py
@@ -14,23 +14,31 @@ class ReservaCommand(Command):
         livro = sys.get_livro_por_id(id_usuario)
 
         if usuario == None:
-            return "Não existe usuário com esse Id!"
+            return "Não existe usuário com esse Id."
 
         if livro == None:
-            return "Não existe livro com esse Id!"
-        
-        ReservaCommand.id_counter += 1
-        cur_id = ReservaCommand.id_counter
-
-        novaReserva = Reserva(cur_id, id_usuario, id_livro)
-
-
-        usuario.adiciona_reserva_na_lista(novaReserva)
-
+            return "Não existe livro com esse Id."
         
 
+        exemp = livro.get_quatidade_exemplares() 
+        
 
+        if( exemp > 0):
+        
+            ReservaCommand.id_counter += 1
+            
+            cur_id = ReservaCommand.id_counter
 
+            novaReserva = Reserva(cur_id, id_usuario, id_livro)
+
+            usuario.adiciona_reserva_na_lista(novaReserva)
+
+            livro.set_quatidade_exemplares(exemp - 1)
+        
+        else:
+            print(f"Não há mais exemplares disponíveis do livro{livro.get_titulo()}.")
+
+        return None
 
 
 
