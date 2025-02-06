f54ea3be4da8b92ae2c41a9bb287547c598529cc
arthursilva35 <arthurss802@gmail.com>
2025-02-06 00:08:48 -0300

    adicionando comando usu para consulta de informacoes de usuario

--- console.py
+++ console.py
@@ -1,13 +1,15 @@
 from classes_command.emprestimo_command import EmprestimoCommand
 from classes_command.reserva_command import ReservaCommand
 from classes_command.devolucao_command import DevolucaoCommand
+from classes_command.consulta_command import ConsultaCommand
 
 class Console:
     def __init__(self):
         self.comandos = {
             "emp": EmprestimoCommand(),
             "res": ReservaCommand(),
-            "dev": DevolucaoCommand()
+            "dev": DevolucaoCommand(),
+            "usu": ConsultaCommand(),
         }
     
     def iniciar(self):
