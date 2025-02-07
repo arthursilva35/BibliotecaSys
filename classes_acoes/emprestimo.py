28c4fc824210194c87042147317d864009c9e312
arthursilva35 <arthurss802@gmail.com>
2025-02-07 11:58:56 -0300

    adicionando atirbuto entrega prevista

--- classes_acoes/emprestimo.py
+++ classes_acoes/emprestimo.py
@@ -2,12 +2,13 @@ import datetime
 from  classes_acoes.IAcaoLivro import IAcaoLivro
 
 class Emprestimo(IAcaoLivro):
-    def __init__(self, id, id_usuario_responsavel, id_livro):
+    def __init__(self, id, id_usuario_responsavel, id_livro, entrega_prevista):
         self._id = id
         self._id_usuario_responsavel = id_usuario_responsavel
         self._id_livro = id_livro
         self._data = datetime.datetime.now()
         self._status = "ativo"
+        self._entrega_prevista = entrega_prevista
 
     def get_id(self):
         return self._id
@@ -35,3 +36,6 @@ class Emprestimo(IAcaoLivro):
             self._status = "ativo"
         
         return None
+    
+    def get_entrega_prevista(self):
+        return self._entrega_prevista
