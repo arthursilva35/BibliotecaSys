8a012acc83dc24f600482691320f412b49884d48
arthursilva35 <arthurss802@gmail.com>
2025-02-07 14:13:59 -0300

    corrigindo devolucao

--- gerenciador_de_emprestimos.py
+++ gerenciador_de_emprestimos.py
@@ -38,10 +38,24 @@ class GerenciadorEmprestimos:
         emp = Emprestimo(cur_id, usuario.get_id(), livro.get_id(), data_entrega)
         usuario.adicionar_emprestimo_ativo(emp)
 
-        print(f"{usuario.get_nome()} pegou emprestado {livro.get_titulo()} com entrega prevista para {data_entrega}")
+        print(f"{usuario.get_nome()} pegou emprestado {livro.get_titulo()} com entrega prevista para {data_entrega}.")
 
         return None
     
     @staticmethod
     def devolver_livro(usuario, livro):
-       pass
+        
+        for emp in usuario.get_emprestimos_ativos():
+            
+            if emp.get_id_usuario_responsavel() == usuario.get_id() and emp.get_id_livro() == livro.get_id():
+
+                usuario.remover_emprestimo_ativo(emp)
+                emp.mudar_status()
+                
+                print(f"Livro {livro.get_titulo()} devolvido por {usuario.get_nome()}.")
+
+                return None
+        
+        print(f"Não há empréstimo de {livro.get_titulo()} feito por {usuario.get_nome()}.")
+
+        return None
